7663a3268fb920c1482b2903934b50a1db3d98f7
valery.hww <valery.hww@gmail.com>
2020-12-15 21:23:29 +0300

    Delete build

--- build/tarablessd1306/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/tarablessd1306
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/tarablessd1306 -ltarablessd1306
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += tarablessd1306
-COMPONENT_LDFRAGMENTS += 
-component-tarablessd1306-build: 
