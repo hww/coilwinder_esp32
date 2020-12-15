a2974fb652da245ddadc5b153281c1ffa72f9b73
valery.hww <valery.hww@gmail.com>
2020-12-15 21:22:04 +0300

    First init

---
+++ build/tarablessd1306/component_project_vars.mk
@@ -0,0 +1,8 @@
+# Automatically generated build file. Do not edit.
+COMPONENT_INCLUDES += $(PROJECT_PATH)/components/tarablessd1306
+COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/tarablessd1306 -ltarablessd1306
+COMPONENT_LINKER_DEPS += 
+COMPONENT_SUBMODULES += 
+COMPONENT_LIBRARIES += tarablessd1306
+COMPONENT_LDFRAGMENTS += 
+component-tarablessd1306-build: 
